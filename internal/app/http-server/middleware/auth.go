1a0d4da3926ddddb73ae64fbee7315fa4f02b9a8
Greench-WaD <spartak-992010@yandex.ru>
2024-11-28 09:28:33 +0600

    fix middleware

--- internal/app/http-server/middleware/auth.go
+++ internal/app/http-server/middleware/auth.go
@@ -7,12 +7,13 @@ import (
 	"time"
 )
 
-func setCookie(w http.ResponseWriter, cipher *ci.Cipher) {
+func setCookie(w http.ResponseWriter, r *http.Request, cipher *ci.Cipher) {
 	c := http.Cookie{
 		Name:    "token",
 		Value:   cipher.Sile(shortuuid.New()),
 		Expires: time.Now().Add(time.Minute * 15),
 	}
+	r.Header.Add("Cookie", c.String())
 	http.SetCookie(w, &c)
 }
 
@@ -21,11 +22,12 @@ func Authentication(cipher *ci.Cipher) func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			token, err := r.Cookie("token")
 			if err != nil {
-				setCookie(w, cipher)
+				setCookie(w, r, cipher)
+
 			} else {
 				_, err := cipher.Open(token.Value)
 				if err != nil {
-					setCookie(w, cipher)
+					setCookie(w, r, cipher)
 				}
 			}
 			next.ServeHTTP(w, r)
